76e710c33ca276d77dc6ceb16b5c8de59b502270
divawidia <wiartha2001@gmail.com>
2023-11-13 00:00:00 +0000

    create BlogCategory CRUD and Blog CRUD

--- app/Http/Controllers/Admin/BlogCategoryController.php
+++ app/Http/Controllers/Admin/BlogCategoryController.php
@@ -3,8 +3,118 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\BlogCategory;
+use App\Models\VehiclePhoto;
+use App\Models\VehicleType;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+use Yajra\DataTables\Facades\DataTables;
 
 class BlogCategoryController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        if (request()->ajax()) {
+            $query = BlogCategory::query();
+
+            return Datatables::of($query)
+                ->addColumn('action', function ($item) {
+                    return '
+                        <div class="dropdown">
+                            <button class="btn btn-light btn-sm dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <i class="bx bx-dots-horizontal-rounded"></i>
+                            </button>
+                            <ul class="dropdown-menu dropdown-menu-end">
+                                <li>
+                                    <a class="dropdown-item" href="' . route('kategori-blog.edit', $item->id) . '">Edit</a>
+                                </li>
+                                <li>
+                                <form action="' . route('kategori-blog.destroy', $item->id) . '" method="POST">
+                                    ' . method_field('delete') . csrf_field() . '
+                                    <button type="submit" class="dropdown-item">
+                                        Hapus
+                                    </button>
+                                </form>
+                                </li>
+                            </ul>
+                        </div>';
+                })
+                ->rawColumns(['action'])
+                ->make();
+        }
+
+        return view('pages.admin.blog-category.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('pages.admin.blog-category.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $data = $request->all();
+
+        $data['slug'] = Str::slug($request->category_name);
+
+        BlogCategory::create($data);
+
+        return redirect()->route('kategori-blog.index')->with('status', 'Data kategori blog berhasil ditambahkan!');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $item = BlogCategory::findOrFail($id);
+
+        return view('pages.admin.blog-category.edit', [
+            'item' => $item
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $data = $request->all();
+
+        $data['slug'] = Str::slug($request->category_name);
+
+        $item = BlogCategory::findOrFail($id);
+        $item->update($data);
+
+        return redirect()->route('kategori-blog.index')->with('status', 'Data kategori blog berhasil diupdate!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $item = BlogCategory::findOrFail($id);
+        $item->delete();
+
+        return redirect()->route('kategori-blog.index')->with('status', 'Data kategori blog berhasil dihapus!');
+    }
 }
