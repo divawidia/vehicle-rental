43da8a07e7888c4dc52e47ea96eed2feb05be5c8
divawidia <wiartha2001@gmail.com>
2023-11-26 00:00:00 +0000

    create vehicle_features table migration

---
+++ database/migrations/2024_02_03_060802_create_vehicle_features_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vehicle_features', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('vehicle_id');
+            $table->string('feature');
+            $table->timestamps();
+            $table->foreign('vehicle_id')->references('id')->on('vehicles')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vehicle_features');
+    }
+};
