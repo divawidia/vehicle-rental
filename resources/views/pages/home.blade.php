7673410264b6230d60222bb91eadd693fecc9eb8
divawidia <wiartha2001@gmail.com>
2023-12-03 00:00:00 +0000

    create more delivery renturn option

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -58,12 +58,19 @@
                                 <div class="spacer-20"></div>
 
                                 <div class="row">
-                                    <div class="col-lg-6 mb20">
-                                        <h5>Pick Up Location</h5>
+                                    <div class="col-lg-6">
+                                        <h5>Delivery Location</h5>
+                                        <select class="form-select" required name="pickup_location_type" id="pickup_location_type">
+                                            <option selected disabled>Choose desired location</option>
+                                            <option value="office">Batur Sari Rental Office (Gg. Beji, Seminyak, Kuta)</option>
+                                            <option value="hotel_villa">Hotel/Villa</option>
+                                            <option value="custom_address">My own address (Pinpoint Map)</option>
+                                        </select>
+                                        <h5 id="pickupAddressLabel" class="mt-3">Address of my villa / hotel :</h5>
                                         <input
                                             type="text"
                                             name="pick_up_loc"
-                                            placeholder="Enter your pickup location"
+                                            placeholder="Enter delivery location"
                                             id="autocomplete_pickup"
                                             autocomplete="on"
                                             class="form-control"
@@ -79,13 +86,19 @@
                                             <input type="text" name="longitude_pickup" id="longitude_pickup" class="form-control">
                                         </div>
                                     </div>
-
-                                    <div class="col-lg-6 mb20">
-                                        <h5>Drop Off Location</h5>
+                                    <div class="col-lg-6">
+                                        <h5>Return Location</h5>
+                                        <select class="form-select" required name="return_location_type" id="return_location_type">
+                                            <option selected disabled>Choose desired location</option>
+                                            <option value="office">Batur Sari Rental Office (Gg. Beji, Seminyak, Kuta)</option>
+                                            <option value="hotel_villa">Hotel/Villa</option>
+                                            <option value="custom_address">My Own Address (Pinpoint Map)</option>
+                                        </select>
+                                        <h5 id="returnAddressLabel" class="mt-3">Address of my villa / hotel :</h5>
                                         <input
                                             type="text"
                                             name="return_loc"
-                                            placeholder="Enter your dropoff location"
+                                            placeholder="Enter return location"
                                             id="autocomplete_return"
                                             autocomplete="on"
                                             class="form-control"
@@ -101,17 +114,31 @@
                                             <input type="text" name="longitude_return" id="longitude_return" class="form-control">
                                         </div>
                                     </div>
-
+                                </div>
+                                <div class="row">
+                                    <div class="col-12 mb20">
+                                        <div id="mapLabel">
+                                            <h5 class="mt-4">Pinpoint my location (delivery & return):</h5>
+                                            <p class="mt-n3">*Drag the marker point to set your location</p>
+                                        </div>
+                                        <div id="map" class="mt-3" style="height: 280px; border-radius:15px;"></div>
+                                        <div id="infowindow-content">
+                                            <span id="place-name" class="title"></span><br />
+                                            <span id="place-address"></span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
                                     <div class="col-lg-6 mb20">
                                         <h5>Pick Up Date & time</h5>
-                                            <input
-                                                type="datetime-local"
-                                                class="form-control"
-                                                id="pick_up_datetime"
-                                                name="pick_up_datetime"
-                                                value="{{ old('pick_up_datetime') }}"
-                                                required
-                                            />
+                                        <input
+                                            type="datetime-local"
+                                            class="form-control"
+                                            id="pick_up_datetime"
+                                            name="pick_up_datetime"
+                                            value="{{ old('pick_up_datetime') }}"
+                                            required
+                                        />
                                     </div>
                                     <div class="col-lg-6 mb20">
                                         <h5>Drop Off Date & Time</h5>
@@ -576,36 +603,172 @@
 @endsection
 
 @push('addon-script')
-    <script src="https://maps.google.com/maps/api/js?key=AIzaSyBzmaIUkgLYiiWK_0tbyqbx31ZsmyA0uoY&libraries=places&callback=initAutocomplete" type="text/javascript"></script>
-{{--    <script async--}}
-{{--            src="https://maps.google.com/maps/api/js?key=AIzaSyBzmaIUkgLYiiWK_0tbyqbx31ZsmyA0uoY&loading=async&libraries=places&callback=initAutocomplete">--}}
-{{--    </script>--}}
+    <script async src="https://maps.google.com/maps/api/js?key=AIzaSyBzmaIUkgLYiiWK_0tbyqbx31ZsmyA0uoY&callback=initMap&libraries=places,maps,marker&v=weekly"
+    >
+    </script>
     <script>
-        google.maps.event.addDomListener(window, 'load', initialize);
-
-        function initialize() {
-            var input_pickup = document.getElementById('autocomplete_pickup');
-            var input_return = document.getElementById('autocomplete_return');
-            var autocomplete_pickup = new google.maps.places.Autocomplete(input_pickup);
-            var autocomplete_return = new google.maps.places.Autocomplete(input_return);
-            autocomplete_pickup.setComponentRestrictions({
-                country: 'id',
+        $(document).ready(function () {
+            $('#autocomplete_pickup').hide();
+            $('#pickupAddressLabel').hide();
+            $('#returnAddressLabel').hide();
+            $('#mapLabel').hide();
+            $('#map').hide();
+            $('#infowindow-content').hide();
+            $('#autocomplete_return').hide();
+            $('#pickup_location_type').on('change', function () {
+                if (document.getElementById("pickup_location_type").value === 'office'){
+                    $('#pickupAddressLabel').hide();
+                    $('#returnAddressLabel').hide();
+                    $('#autocomplete_pickup').val('Batur Sari Rental, Gang Beji, Seminyak, Badung Regency, Bali, Indonesia').hide();
+                    $('#autocomplete_return').val('Batur Sari Rental, Gang Beji, Seminyak, Badung Regency, Bali, Indonesia').hide();
+                    $('#latitude_pickup').val(-8.6836849);
+                    $('#longitude_pickup').val(115.1631064);
+                    $('#latitude_return').val(-8.6836849);
+                    $('#longitude_return').val(115.1631064);
+                    $('#mapLabel').hide();
+                    $('#map').hide();
+                    $('#infowindow-content').hide();
+                    $('#return_location_type').val('office');
+                } else if (document.getElementById("pickup_location_type").value === 'hotel_villa'){
+                    $('#pickupAddressLabel').show();
+                    $('#returnAddressLabel').show();
+                    $('#autocomplete_pickup').val('').show();
+                    $('#autocomplete_return').val('').show();
+                    $('#latitude_pickup').val('');
+                    $('#longitude_pickup').val('');
+                    $('#latitude_return').val('');
+                    $('#longitude_return').val('');
+                    $('#mapLabel').hide();
+                    $('#map').hide();
+                    $('#infowindow-content').hide();
+                    $('#return_location_type').val('hotel_villa');
+                } else if (document.getElementById("pickup_location_type").value === 'custom_address'){
+                    $('#pickupAddressLabel').hide();
+                    $('#returnAddressLabel').hide();
+                    $('#autocomplete_pickup').val('').hide();
+                    $('#autocomplete_return').val('').hide();
+                    $('#infowindow-content-pickup').show();
+                    $('#infowindow-content-return').show();
+                    $('#latitude_pickup').val('');
+                    $('#longitude_pickup').val('');
+                    $('#latitude_return').val('');
+                    $('#longitude_return').val('');
+                    $('#mapLabel').show();
+                    $('#map').show();
+                    $('#infowindow-content').show();
+                    $('#return_location_type').val('custom_address');
+                }
             });
-            autocomplete_return.setComponentRestrictions({
-                country: 'id',
+            $('#return_location_type').on('change', function () {
+                if (document.getElementById("return_location_type").value === 'office'){
+                    $('#returnAddressLabel').hide();
+                    $('#autocomplete_return').val('Batur Sari Rental, Gang Beji, Seminyak, Badung Regency, Bali, Indonesia').hide();
+                    $('#latitude_return').val(-8.6836849);
+                    $('#longitude_return').val(115.1631064);
+                    $('#mapLabel').hide();
+                    $('#map').hide();
+                    $('#infowindow-content').hide();
+                } else if (document.getElementById("return_location_type").value === 'hotel_villa'){
+                    $('#returnAddressLabel').show();
+                    $('#autocomplete_return').val('').show();
+                    $('#latitude_return').val('');
+                    $('#longitude_return').val('');
+                    $('#mapLabel').hide();
+                    $('#map').hide();
+                    $('#infowindow-content').hide();
+                } else if (document.getElementById("return_location_type").value === 'custom_address'){
+                    $('#returnAddressLabel').hide();
+                    $('#autocomplete_pickup').val('').hide();
+                    $('#autocomplete_return').val('').hide();
+                    $('#infowindow-content-return').show();
+                    $('#latitude_return').val('');
+                    $('#longitude_return').val('');
+                    $('#mapLabel').show();
+                    $('#map').show();
+                    $('#infowindow-content').show();
+                    $('#pickup_location_type').val('custom_address');
+                }
+            });
+        });
+
+        function initMap() {
+            var map = new google.maps.Map(document.getElementById("map"), {
+                center: { lat: -8.6836849, lng: 115.1631064 },
+                zoom: 13
             });
 
-            autocomplete_pickup.addListener('place_changed', function() {
-                var place = autocomplete_pickup.getPlace();
+            let input_pickup = document.getElementById("autocomplete_pickup");
+            let input_return = document.getElementById("autocomplete_return");
+
+            let autocomplete_pickup = new google.maps.places.Autocomplete(input_pickup);
+            let autocomplete_return = new google.maps.places.Autocomplete(input_return);
+
+            autocomplete_pickup.bindTo("bounds", map);
+
+            let infowindow = new google.maps.InfoWindow();
+            let infowindowContent = document.getElementById("infowindow-content");
+
+            infowindow.setContent(infowindowContent);
+
+            let marker = new google.maps.Marker({
+                map,
+                position: { lat: -8.6836849, lng: 115.1631064 },
+                anchorPoint: new google.maps.Point(0, -29)
+            });
+            marker.setVisible(true);
+            marker.setDraggable(true);
+
+            autocomplete_pickup.addListener("place_changed", () => {
+                infowindow.close();
+                marker.setVisible(false);
+
+                let place = autocomplete_pickup.getPlace();
+
+                if (!place.geometry || !place.geometry.location) {
+                    window.alert("No details available for input: '" + place.name + "'");
+                    return;
+                }
+
+                if (place.geometry.viewport) {
+                    map.fitBounds(place.geometry.viewport);
+                } else {
+                    map.setCenter(place.geometry.location);
+                    map.setZoom(17);
+                }
+
+                marker.setPosition(place.geometry.location);
+                marker.setVisible(true);
+                infowindowContent.children["place-name"].textContent = place.name;
+                infowindowContent.children["place-address"].textContent = place.formatted_address;
+                infowindow.open(map, marker);
                 $('#latitude_pickup').val(place.geometry['location'].lat());
                 $('#longitude_pickup').val(place.geometry['location'].lng());
-            });
-            autocomplete_return.addListener('place_changed', function() {
-                var place = autocomplete_return.getPlace();
                 $('#latitude_return').val(place.geometry['location'].lat());
                 $('#longitude_return').val(place.geometry['location'].lng());
+                input_return.value = input_pickup.value;
+            });
+
+            autocomplete_return.addListener("place_changed", () => {
+                let place = autocomplete_return.getPlace();
+                $('#latitude_return').val(place.geometry['location'].lat());
+                $('#longitude_return').val(place.geometry['location'].lng());
+
+            });
+            marker.addListener('dragend', () =>{
+                infowindow.close();
+                const latLang = marker.getPosition();
+                infowindowContent.children["place-name"].textContent = latLang;
+                infowindowContent.children["place-address"].textContent = '';
+                infowindow.open(map, marker);
+                input_pickup.value = '';
+                input_return.value = '';
+                $('#latitude_pickup').val(latLang.lat());
+                $('#longitude_pickup').val(latLang.lng());
+                $('#latitude_return').val(latLang.lat());
+                $('#longitude_return').val(latLang.lng());
             });
         }
+        initMap();
     </script>
     <script>
         var date = new Date().toISOString().slice(0,new Date().toISOString().lastIndexOf(":"));
