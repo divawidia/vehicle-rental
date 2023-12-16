b28e96a7e9d4266e3867532c17af42c75dc6bb4c
divawidia <wiartha2001@gmail.com>
2023-12-16 00:00:00 +0000

    create migration to add field in vehicles table

---
+++ database/migrations/2024_02_25_014530_add_field_in_vehicles_tablke.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('vehicles', function (Blueprint $table) {
+            $table->enum('discount_status', [1,0])->nullable()->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('vehicles', function (Blueprint $table) {
+            $table->dropColumn('discount_status');
+        });
+    }
+};
