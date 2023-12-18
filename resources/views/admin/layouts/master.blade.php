f06da77c8f110ac19f43c04ed3ac9078cc1fd716
divawidia <wiartha2001@gmail.com>
2023-12-18 00:00:00 +0000

    refactor (vite config): update assets path

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -15,9 +15,9 @@
     <link href="https://cdn.datatables.net/v/bs5/jq-3.7.0/dt-1.13.8/datatables.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet"/>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.3.0/dist/select2-bootstrap-5-theme.min.css"/>
-    <!-- Or for RTL support -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.3.0/dist/select2-bootstrap-5-theme.rtl.min.css"/>
     @stack('addon-style')
+
+    @vite(['resources/assets/admin/css/app.min.css', 'resources/assets/admin/css/bootstrap.min.css', 'resources/assets/admin/js/app.js', ])
 </head>
 
 @yield('body')
--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/sass/app.scss',
-                'resources/js/app.js',
+                'resources/assets/admin/css/app.css',
+                'resources/assets/admin/js/app.js',
             ],
             refresh: true,
         }),
