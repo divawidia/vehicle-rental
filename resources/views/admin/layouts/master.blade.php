510b7a45b0bfebce009daf630781d401d37d384b
divawidia <wiartha2001@gmail.com>
2023-12-19 00:00:00 +0000

    refactor (admin master layout): delete unused tags

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -13,9 +13,6 @@
     <!-- include head css -->
     @stack('prepend-style')
     @include('admin.includes.head-css')
-{{--    <link href="https://cdn.datatables.net/v/bs5/jq-3.7.0/dt-1.13.8/datatables.min.css" rel="stylesheet">--}}
-{{--    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet"/>--}}
-{{--    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.3.0/dist/select2-bootstrap-5-theme.min.css"/>--}}
     @stack('addon-style')
 
     @vite([
@@ -44,7 +41,6 @@
     @stack('prepend-script')
     @include('sweetalert::alert')
     @include('admin.includes.vendor-scripts')
-{{--    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>--}}
     @stack('addon-script')
 </body>
 
