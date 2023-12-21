f764866e30334562fe355ccf78afaa0472861c7c
divawidia <wiartha2001@gmail.com>
2023-12-21 00:00:00 +0000

    refactor (admin css): add additional css cdn

--- resources/views/admin/includes/head-css.blade.php
+++ resources/views/admin/includes/head-css.blade.php
@@ -1,2 +1,6 @@
 @yield('css')
 <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+<link href="https://cdn.datatables.net/v/bs5/jq-3.7.0/dt-1.13.8/datatables.min.css" rel="stylesheet">
+<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.3.0/dist/select2-bootstrap-5-theme.min.css" />
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/metismenujs/dist/metismenujs.min.css">
