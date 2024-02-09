2e85ae4055bb8029e277a4273ac5e6d4041d20bb
divawidia <wiartha2001@gmail.com>
2024-02-09 21:22:13 +0800

    create filter vehicle

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,7 +5,10 @@ namespace App\Http\Controllers;
 use App\Models\Blog;
 use App\Models\Gallery;
 use App\Models\Tag;
+use App\Models\Transmission;
 use App\Models\Vehicle;
+use App\Models\VehicleBrand;
+use App\Models\VehicleType;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -29,10 +32,76 @@ class HomeController extends Controller
             'vehicles' => $vehicles
         ]);
     }
-    public function vehicleList()
+    public function vehicleList(Request $request)
     {
-        $vehicles = Vehicle::with('photos', 'transmission', 'vehicle_type', 'brand')->get();
-        return view('pages.vehicle-list', compact('vehicles'));
+        $vehicleTypes = VehicleType::all();
+        $transmissions = Transmission::all();
+        $brands = VehicleBrand::all();
+//        dd(array($request->vehicle_type_id));
+        if ($request->all() == []) {
+            $previousData = [
+                'vehicle_type_id' => [],
+                'transmission_id' => [],
+                'brand_id' => [],
+                'engine_capacity_min' => 0,
+                'engine_capacity_max' => 2000,
+                'daily_price_min' => 0,
+                'daily_price_max' => 3000000,
+                'monthly_price_min' => 0,
+                'monthly_price_max' => 3000000
+            ];
+        }else{
+            $previousData = [
+                'vehicle_type_id' => ($request->vehicle_type_id == null ? [] : $request->vehicle_type_id),
+                'transmission_id' => ($request->transmission_id == null ? [] : $request->transmission_id),
+                'brand_id' => ($request->brand_id == null ? [] : $request->brand_id),
+                'engine_capacity_min' => 0,
+                'engine_capacity_max' => 2000,
+                'daily_price_min' => 0,
+                'daily_price_max' => 3000000,
+                'monthly_price_min' => 0,
+                'monthly_price_max' => 3000000
+            ];
+        }
+
+        $vehicles = Vehicle::query();
+        if (isset($request->vehicle_type_id) && ($request->vehicle_type_id != null)){
+            $vehicles->whereHas('vehicle_type', function ($vehicle) use ($request){
+                $vehicle->whereIn('id', $request->vehicle_type_id);
+            });
+        }
+        if (isset($request->transmission_id) && ($request->transmission_id != null)){
+            $vehicles->whereHas('transmission', function ($vehicle) use ($request){
+                $vehicle->whereIn('id', $request->transmission_id);
+            });
+        }
+        if (isset($request->brand_id) && ($request->brand_id != null)){
+            $vehicles->whereHas('brand', function ($vehicle) use ($request){
+                $vehicle->whereIn('id', $request->brand_id);
+            });
+        }
+        if (isset($request->engine_capacity_min) && ($request->engine_capacity_min != null)){
+            $vehicles->where('engine_capacity', '>=', $request->engine_capacity_min);
+        }
+        if (isset($request->engine_capacity_max) && ($request->engine_capacity_max != null)){
+            $vehicles->where('engine_capacity', '<=', $request->engine_capacity_max);
+        }
+        if (isset($request->daily_price_min) && ($request->daily_price_min != null)){
+            $vehicles->where('daily_price', '>=', $request->daily_price_min);
+        }
+        if (isset($request->daily_price_max) && ($request->daily_price_max != null)){
+            $vehicles->where('daily_price', '<=', $request->daily_price_max);
+        }
+        if (isset($request->monthly_price_min) && ($request->monthly_price_min != null)){
+            $vehicles->where('monthly_price', '>=', $request->monthly_price_min);
+        }
+        if (isset($request->monthly_price_max) && ($request->monthly_price_max != null)){
+            $vehicles->where('monthly_price', '<=', $request->monthly_price_max);
+        }
+        $vehicles = $vehicles->get();
+
+
+        return view('pages.vehicle-list', compact('vehicles', 'vehicleTypes', 'transmissions', 'brands', 'previousData'));
     }
     public function vehicleDetail(Request $request, string $slug)
     {
@@ -45,6 +114,33 @@ class HomeController extends Controller
 //        dd($vehicleUnit);
         return view('pages.vehicle-detail', compact(['vehicle', 'vehicleUnit', 'booking']));
     }
+
+//    public function filter_vehicle(Request $request)
+//    {
+//        $vehicles = DB::table('vehicles')
+//            ->where(function ($q) use ($request) {
+//
+//                if ($request->has('vehicle_type_id')) {
+//                    foreach ($request->vehicle_type_id as $vehicle_type_id){
+//                        $q->orWhere('vehicle_type_id',$request->vehicle_type_id );
+//                    }
+//                    $q->orWhere('vehicle_type_id',$request->vehicle_type_id )
+//                        ->orWhere('vehicle_type_id',$request->vehicle_type_id )
+//                        ->orWhere('vehicle_type_id',$request->vehicle_type_id )
+//                } else {
+//                    if ($request->has('type')) {
+//                        $q->where($request->type, 'LIKE', '%' . $request->search . '%');
+//                    }
+//                    if ($request->has('object_type')) {
+//                        $q->where('object_type', 'LIKE', '%' . $request->object_type . '%');
+//                    }
+//                    if ($request->has('launch_year')) {
+//                        $q->where('launch', 'LIKE', '%' . $request->launch_year . '%');
+//                    }
+//                }
+//            })
+//        return view('search_result',compact('all_products'))->render();
+//    }
 
     public function blogList()
     {
@@ -68,10 +164,9 @@ class HomeController extends Controller
         return view('pages.gallery', compact('galleries'));
     }
 
-    public function bookingPage(){
-        $vehicles = Vehicle::with('photos', 'transmission', 'vehicle_type', 'brand')->get();
-        return view('pages.booking',[
-            'vehicles' => $vehicles
-        ]);
+    public function bookingPage(Request $request){
+        $booking = $request->session()->get('booking');
+        $vehicleTypes = VehicleType::all();
+        return view('pages.booking', compact(['booking', 'vehicleTypes']));
     }
 }
