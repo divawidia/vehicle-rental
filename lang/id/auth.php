8cd684195e44326bde32f5d6c7be99473c68af5d
divawidia <wiartha2001@gmail.com>
2025-05-30 13:31:10 +0800

    refactor (id lang): update auth lang

--- lang/id/auth.php
+++ lang/id/auth.php
@@ -13,8 +13,8 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'password' => 'The provided password is incorrect.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'Kredensial ini tidak cocok dengan yang ada di database.',
+    'password' => 'Kata sandi yang dimasukan tidak benar.',
+    'throttle' => 'Terlalu banyak percobaan masuk. Silakan coba lagi dalam :seconds detik.',
 
 ];
